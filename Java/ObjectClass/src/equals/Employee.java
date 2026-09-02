d9077e89211fa89096ab5d132b6f651bf816f024
tarunrb02 <tarunrbombalekar@gmail.com>
2026-09-02 21:06:58 +0530

    Day48

---
+++ Java/ObjectClass/src/equals/Employee.java
@@ -0,0 +1,32 @@
+package equals;
+
+public class Employee {
+	int id;
+	String name;
+	
+	Employee(int id, String name){
+		this.name=name;
+		this.id=id;
+	}
+	
+	public boolean equals(Object o) {
+		if(o instanceof Employee) {
+			Employee e= (Employee) o;
+			if(this.id==e.id && this.name==e.name)
+				return true;
+
+		}
+		return false;
+	}
+	
+	public boolean equals(Employee e) {
+		return (this.id==e.id && this.name.equals(e.name));
+	}
+	s
+	public static void main(String[] args) {
+		Employee e1=new Employee(1, "Tarun");
+		Employee e2=new Employee(1, "Tarun");
+		System.out.println(e1.equals(e2));
+	}
+
+}
---
+++ Java/ObjectClass/src/hashCode/Student.java
@@ -0,0 +1,26 @@
+package hashCode;
+
+import java.util.Objects;
+
+public class Student {
+	int id;
+	String name;
+	
+	Student(int id, String name){
+		this.id=id;
+		this.name=name;
+	}
+	
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(id,name);
+//	}
+	
+	public static void main(String[] args) {
+		Student s1=new Student(1,"tarun");
+		Student s2=new Student(1,"tarun");
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+	}
+
+}
